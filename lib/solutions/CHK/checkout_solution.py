872e5c629aaa7d4389bbac6dd9a5172face7cd03
sbx_user1051 <sbx_user1051@169.254.113.53>
2024-11-01 16:06:02 +0000

    Fri Nov 01 16:06:02 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -11,27 +11,15 @@
 | C    | 20    |                |
 | D    | 15    |                |
 +------+-------+----------------+
-
-new table
-
-+------+-------+------------------------+
-| Item | Price | Special offers         |
-+------+-------+------------------------+
-| A    | 50    | 3A for 130, 5A for 200 |
-| B    | 30    | 2B for 45              |
-| C    | 20    |                        |
-| D    | 15    |                        |
-| E    | 40    | 2E get one B free      |
-+------+-------+------------------------+
 """
 from tabnanny import check
 
-price_table = {'3A': 130, '5A': 200 ,'2B': 45, 'A': 50, 'B': 30, 'C': 20, 'D': 15, 'E': 40 }
+price_table = {'3A': 130,'2B': 45, 'A': 50, 'B': 30, 'C': 20, 'D': 15 }
 
 def checkout(skus:str) -> int:
 
     result = 0
-    count = {'3A':0, '5A':0, '2B':0}
+    count = {'3A':0, '2B':0}
 
     for item in skus:
         if item in price_table:
@@ -45,21 +33,12 @@ def checkout(skus:str) -> int:
     if result != -1:
 
         for key, value in count.items():
-            if key == 'A' and value % 3 == 0 or value % 5 == 0 and value % 3 == 0:
+            if key == 'A':
                 count['3A'] = int(value / 3)
                 count['A'] = value % 3
-
-            if key == 'A' and value % 5 == 0:
-                count['5A'] = int(value / 5)
-                count['A'] = value % 5
-
             elif key == 'B':
                 count['2B'] = int(value / 2)
                 count['B'] = value % 2
-
-            elif key == 'E':
-                count['E'] = int(value / 2)
-                count['E'] = value % 2
 
         result = sum([price_table[item] * count[item] for item in count])
 
