f4b7e1f49934131a5f4cc2dcbd364baabf1bb9aa
sbx_user1051 <sbx_user1051@169.254.113.53>
2024-11-01 16:39:14 +0000

    Fri Nov 01 16:39:14 UTC 2024

--- test/solution_tests/CHK_1/test_chk_1.py
+++ test/solution_tests/CHK_1/test_chk_1.py
@@ -27,5 +27,5 @@ class TestCheckout():
     def test_new_discount(self):
         assert checkout_solution.checkout('AAAAA') == 200
 
-    def test_new_discount(self):
-        assert checkout_solution.checkout('AAAAAAAA') == 200
+    def test_new_discount_multiple(self):
+        assert checkout_solution.checkout('AAAAAAAA') == 430
