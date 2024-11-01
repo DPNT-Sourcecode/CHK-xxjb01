9c494df30578854e17653e96d4b536d09291c376
sbx_user1051 <sbx_user1051@169.254.113.53>
2024-11-01 14:44:56 +0000

    Fri Nov 01 14:44:56 UTC 2024

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,6 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    return "Hello, World!"
+def hello(friend_name:str) -> str:
+    return f"Hello, {friend_name}!"
 
