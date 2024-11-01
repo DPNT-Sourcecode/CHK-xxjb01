1c1b34bdea2e9ba55f40bfdd700cca099e3ccf23
sbx_user1051 <sbx_user1051@169.254.113.53>
2024-11-01 14:30:00 +0000

    Fri Nov 01 14:30:00 UTC 2024

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,10 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+    def test_negative_sum(self):
+        assert sum_solution.compute(-2,-2) == -4
+
+    def test_zero_sum(self):
+        assert sum_solution.compute(0, 0) == 0
+
