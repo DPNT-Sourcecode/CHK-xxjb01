45ea37d13d2d02d2da5fd556ffde6322f631e5c9
sbx_user1051 <sbx_user1051@169.254.131.181>
2024-11-03 14:24:03 +0000

    Sun Nov 03 14:24:03 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -130,15 +130,15 @@ def checkout(skus:str) -> int:
         nonlocal discount
         value = count['R']
         counter = value
+        target_q = count['Q']
         if value >= 3:
             while counter >= 3:
                 counter -= 3
                 discount -= 30
+
                 total_q = count['Q']
                 if total_q == 0:
                     count['Q'] += 1
-                elif total_q % 3 == 0:
-                    count['Q'] -= 1
 
     for item in skus:
         if item in price_table:
@@ -170,3 +170,4 @@ def checkout(skus:str) -> int:
     return result
 
 checkout('RRRRRRRQQQQQQ')
+
