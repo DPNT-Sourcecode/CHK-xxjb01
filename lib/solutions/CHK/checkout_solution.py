44a7d746aa66916838576cdcade3d88a43058cbc
sbx_user1051 <sbx_user1051@169.254.62.141>
2024-11-03 15:22:27 +0000

    Sun Nov 03 15:22:27 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -142,7 +142,11 @@ def checkout(skus:str) -> int:
 
 
     def multi_discount():
-        pass
+        all_items_in_discount = ['S','T','X','Y','Z']
+        total_items = sum([count[dis_item] for dis_item in all_items_in_discount])
+        count['3Q'] = int(value / 3)
+        count['Q'] = value % 3
+
 
     for item in skus:
         if item in price_table:
@@ -175,4 +179,3 @@ def checkout(skus:str) -> int:
     print(f'COUNT: {count}')
 
     return result
-
